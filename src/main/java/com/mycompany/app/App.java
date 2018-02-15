af810221bb6ef35f726770d7ced296f36d713222
Merve Mallı <mervemalli97@gmail.com>
2018-02-15 23:24:44 +0300

    pom modified

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,14 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public static int search(int[] arr1, int[] arr2, int first, int second){
+        return arr1[first] + arr2[second];
     }
 }
