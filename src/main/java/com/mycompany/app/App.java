10fb0a376b808509dd2d96ea838338856179ea8f
Merve Mallı <mervemalli97@gmail.com>
2018-02-16 05:23:56 +0300

    App.java updated for Spark

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,24 +1,99 @@
 package com.mycompany.app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
 public class App 
 {
     
-    public static boolean decrypt(int[] crypted, int[] solved, int add, int mult){
-    	if (crypted == null){
-    		if (solved != null)
-    			return false;
-    		else return true;
+    public static boolean decrypt(ArrayList<Integer> crypted, ArrayList<Integer> solved, int add, int mult){
+    	if (crypted.isEmpty()){
+    		if (solved.isEmpty())
+    			return true;
+    		return false;
     	}
     	
-    	if (crypted.length != solved.length)
+    	if (crypted.size() != solved.size())
     		return false;
     	
-        for (int i = 0; i < crypted.length; i++){
-        	if ((crypted[i]+add)*mult != solved[i])
+        for (int i = 0; i < crypted.size(); i++){
+        	if ((crypted.get(i)+add)*mult != solved.get(i))
         		return false;
         }
         return true;
     }
+    
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+        	//System.out.println(req.queryParams("input1"));
+        	//System.out.println(req.queryParams("input2"));
+
+        	String input1 = req.queryParams("input1");
+        	java.util.Scanner sc1 = new java.util.Scanner(input1);
+        	sc1.useDelimiter("[;\r\n]+");
+        	java.util.ArrayList<Integer> inputList1 = new java.util.ArrayList<>();
+        	while (sc1.hasNext())
+        	{
+        		int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+        		inputList1.add(value);
+        	}
+        	System.out.println(inputList1);
+        	sc1.close();
+        	
+        	String input2 = req.queryParams("input2");
+        	java.util.Scanner sc2 = new java.util.Scanner(input2);
+        	sc2.useDelimiter("[;\r\n]+");
+        	java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+        	while (sc1.hasNext())
+        	{
+        		int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+        		inputList2.add(value);
+        	}
+        	System.out.println(inputList2);
+        	sc2.close();
+
+
+        	String input3 = req.queryParams("input3").replaceAll("\\s","");
+        	int input3AsInt = Integer.parseInt(input3);
+        	String input4 = req.queryParams("input4").replaceAll("\\s","");
+        	int input4AsInt = Integer.parseInt(input4);
+
+        	boolean result = App.decrypt(inputList1, inputList2, input3AsInt, input4AsInt);
+
+        	Map map = new HashMap();
+        	map.put("result", result);
+        	return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+            	Map map = new HashMap();
+            	map.put("result", "not computed yet!");
+            	return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 }
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,6 +1,7 @@
 package com.mycompany.app;
 
 import junit.framework.Test;
+import java.util.ArrayList;
 import java.util.Arrays;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -20,38 +21,38 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    public void testNullArray()
+    public void testEmptyArray()
     {
-    	int [] arr1 = null;
-    	int [] arr2 = null;
+    	ArrayList<Integer> arr1 = new ArrayList<Integer>();
+    	ArrayList<Integer> arr2 = new ArrayList<Integer>();
         assertTrue(new App().decrypt(arr1, arr2, 0, 1) );
     }
     
     public void testSmallFirstArray()
     {
-    	int [] arr1 = {1,1,1};
-    	int [] arr2 = {1,1,1,1,1};
+    	ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1,1,1));
+    	ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1,1,1,1,1));
         assertFalse(new App().decrypt(arr1, arr2, 0, 1) );
     }
     
     public void testBigFirstArray()
     {
-    	int [] arr1 = {1,1,1,1,1,1,1};
-    	int [] arr2 = {1,1,1,1,1};
+    	ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1,1,1,1,1,1,1));
+    	ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1,1,1,1,1));
         assertFalse(new App().decrypt(arr1, arr2, 0, 1) );
     }
     
     public void testAppropriateArrays()
     {
-    	int [] arr1 = {1,1,1,1,1};
-    	int [] arr2 = {1,1,1,1,1};
+    	ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(1,1,1));
+    	ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1,1,1));
         assertTrue(new App().decrypt(arr1, arr2, 0, 1) );
     }
     
     public void testInappropriateArrays()
     {
-    	int [] arr1 = {1,1,1,1,1};
-    	int [] arr2 = {2,2,2,2,2};
+    	ArrayList<Integer> arr1 = new ArrayList<>(Arrays.asList(2,2,2,2,2));
+    	ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1,1,1,1,1));
         assertFalse(new App().decrypt(arr1, arr2, 0, 1) );
     }
 }
